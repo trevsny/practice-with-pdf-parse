bcff03cf09150f6dc441ae4c8ce8591016dcc391
Trevor Snyder <trevsny@gmail.com>
2018-12-03 23:00:50 -0600

    Added promises.js for practice

--- extractWord.js
+++ extractWord.js
@@ -2,16 +2,11 @@ let letters = ['s','t','r','e','e','t']
 
 let newWord = ['b','a','d']
 
-// console.log(word.join(''));
 let array = []
 let word = letters.join('')
 array.push(word)
 console.log(array)
 
 const pdfWrite = require('pdf-write-page')
-pdfWrite({
-    'in': './../Resume_coverLetters/TrevorResume.pdf',
-    'out': './../Resume_coverLetters/TrevorResume.pdf',
-    'pagenumber': 0
-}).write(1,1,'A');
+pdfWrite({'in':'./../Resume_coverLetters/TrevorResume.pdf', 'out': './../Resume_coverLetters/Modified.pdf', 'pageNumber': 0}).write({'x': 1, 'y': 1, 'txt': 'Hello World'}).cfg({'size': 40}).end();
 
